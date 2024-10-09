6e98ed7ebb7208206a9e635b71a793381f0a5a36
AnastasiaBrinati <birillo347@gmail.com>
2024-10-09 16:43:18 +0200

    compose details

--- helpers/generate_docker_compose.py
+++ helpers/generate_docker_compose.py
@@ -90,6 +90,7 @@ services:
     build:
       context: .
       dockerfile: Dockerfile
+    privileged: true
     command: python server.py --number_of_rounds={args.num_rounds}
     environment:
       FLASK_RUN_PORT: 6000
